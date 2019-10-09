040bb26e6feb7b8905d881e788dccbdca7bded39
ansarisaeem00 <ansarisaeem00@gmail.com>
2019-10-09 23:35:40 +0530

    aac expt2

---
+++ aac/menu.py
@@ -0,0 +1,10 @@
+
+while(True):
+	print("1.Insert node")
+	print("2.Delete node")
+	print("3.Quit")
+	choice = input("Enter your choice")
+	if(choice == "3"):
+		break
+	else if(choice == "1") :
+		data = input ("enter node to be insert")			    	
