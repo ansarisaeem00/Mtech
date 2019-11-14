a571446eb120ca62084a4139f164f25f96e6381a
ansarisaeem00 <ansarisaeem00@gmail.com>
2019-11-14 21:12:17 +0530

    seminar

---
+++ seminar/impl/clustering.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 12 22:57:36 2019
+
+@author: saeem
+"""
+import math
+
+road =int(math.pow(10,9.3))
+parking = int (math.pow(10,8.5) )
+sidewalk = int(math.pow(10,8.2))
+area = int (math.pow(10,8))
+train_track = int (math.pow(10,7) )
+pedestrians = int (math.pow(10,6.8))
+animals = int (math.pow(10,6.5) )
+bike  =int (math.pow(10,7) )
+auto_rikshaw = int (math.pow(10,8.2) )
+car = int (math.pow(10,8.8) )
+truck =int (math.pow(10,7.5) )
+bus = int (math.pow(10,7.5) )
+signal =int (math.pow(10,7.9) )
+population_density = int (math.pow(10,9.3) )
+bridge = int (math.pow(10,7.4) )
+tunnel = int (math.pow(10,6.5) )
+weather = int (math.pow(10,8.5) )
+
+
+print(parking)
